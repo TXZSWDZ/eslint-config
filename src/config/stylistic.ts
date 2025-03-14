9444886c450078c6fa7253e1cc9eb16b3b88df10
WangZe <wangze@zb-software.com>
2025-03-14 13:11:18 +0800

    add stylistic config

---
+++ src/config/stylistic.ts
@@ -0,0 +1,28 @@
+import type { Config, StylisticConfig, OptionsOverrides } from '../types'
+import { interopDefault } from '../utils'
+
+export const StylisticConfigDefaults: StylisticConfig = { indent: 2, jsx: true, quotes: 'single', semi: false }
+
+export async function stylistic(options: StylisticConfig & OptionsOverrides): Promise<Config> {
+  const {
+    indent,
+    jsx,
+    quotes,
+    semi,
+    overrides = {},
+  } = { ...StylisticConfigDefaults, ...options }
+
+  const [pluginStylistic, pluginAntfu] = await Promise.all([interopDefault(import('@stylistic/eslint-plugin')), interopDefault(import('eslint-plugin-antfu'))])
+
+  const customizeConfig = pluginStylistic.configs.customize(({ pluginName: 'style', indent, jsx, quotes, semi }))
+
+  return { plugins: { antfu: pluginAntfu, style: pluginStylistic }, rules: {
+    ...customizeConfig.rules,
+    'antfu/consistent-chaining': 'error',
+    'antfu/consistent-list-newline': 'error',
+    'style/no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'style/generator-star-spacing': ['error', { after: true, before: false }],
+    'style/yield-star-spacing': ['error', { after: true, before: false }],
+    ...overrides,
+  } }
+}
