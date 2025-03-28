bcc32954af63331a15cf923803d8261bd0e5a707
WangZe <wangze@zb-software.com>
2025-03-28 16:43:58 +0800

    feat：提取工具函数

--- src/utils/interopDefault.ts
+++ src/utils/interopDefault.ts
@@ -1,4 +1,4 @@
-import type { Awaitable } from '../../types'
+import type { Awaitable } from '../types'
 export async function interopDefault<T>(m: Awaitable<T>): Promise<T extends { default: infer U } ? U : T> {
   const resolved = await m
   return (resolved as any).default || resolved
