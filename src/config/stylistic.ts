4105c701b9e95ae48e6fa950b09a28a930d9edf2
WangZe <wangze@zb-software.com>
2025-03-31 16:57:56 +0800

    feat：修改stylistic规则配置

--- src/config/stylistic.ts
+++ src/config/stylistic.ts
@@ -1,7 +1,14 @@
-import type { Config, StylisticConfig, OptionsOverrides } from '../types'
+import type { Config, OptionsOverrides, StylisticConfig } from '../types'
+
+import { pluginAntfu } from '../plugins'
 import { interopDefault } from '../utils/index'
 
-export const StylisticConfigDefaults: StylisticConfig = { indent: 2, jsx: true, quotes: 'single', semi: false }
+export const StylisticConfigDefaults: StylisticConfig = {
+  indent: 2,
+  jsx: true,
+  quotes: 'single',
+  semi: false,
+}
 
 export async function stylistic(options: StylisticConfig & OptionsOverrides): Promise<Config> {
   const {
@@ -12,17 +19,33 @@ export async function stylistic(options: StylisticConfig & OptionsOverrides): Pr
     overrides = {},
   } = { ...StylisticConfigDefaults, ...options }
 
-  const [pluginStylistic, pluginAntfu] = await Promise.all([interopDefault(import('@stylistic/eslint-plugin')), interopDefault(import('eslint-plugin-antfu'))])
+  const pluginStylistic = await interopDefault(import('@stylistic/eslint-plugin'))
 
-  const customizeConfig = pluginStylistic.configs.customize(({ pluginName: 'style', indent, jsx, quotes, semi }))
+  const customizeConfig = pluginStylistic.configs.customize(({
+    pluginName: 'style',
+    indent,
+    jsx,
+    quotes,
+    semi,
+  }))
 
-  return { plugins: { antfu: pluginAntfu, style: pluginStylistic }, rules: {
-    ...customizeConfig.rules,
-    'antfu/consistent-chaining': 'error',
-    'antfu/consistent-list-newline': 'error',
-    'style/no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
-    'style/generator-star-spacing': ['error', { after: true, before: false }],
-    'style/yield-star-spacing': ['error', { after: true, before: false }],
-    ...overrides,
-  } }
+  return {
+    plugins: {
+      antfu: pluginAntfu,
+      style: pluginStylistic,
+    },
+    rules: {
+      ...customizeConfig.rules,
+      'antfu/consistent-chaining': 'error',
+      'antfu/consistent-list-newline': 'error',
+      'antfu/curly': 'error',
+      'antfu/if-newline': 'error',
+      'antfu/top-level-function': 'error',
+
+      'style/no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      'style/generator-star-spacing': ['error', { after: true, before: false }],
+      'style/yield-star-spacing': ['error', { after: true, before: false }],
+      ...overrides,
+    },
+  }
 }
